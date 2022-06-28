b2100d9e322f8b24f65e6b469be528fa8805bf64
Szabek <mrschabpl@gmail.com>
2022-06-28 22:36:15 +0200

    simple routing and empty components created

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Route, Routes} from "react-router-dom";
+//pages
+import Navbar from "./components/Navbar";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Error from "./pages/Error";
+//components
+import SingleCocktail from "./pages/SingleCocktail";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Navbar/>
+      <Routes>
+          <Route exact path="/" element={<Home/>}/>
+          <Route exact path="/about" element={<About/>}/>
+          <Route exact path="/coctail/:id" element={<SingleCocktail/>}/>
+          <Route path="*" element={<Error/>}/>
+      </Routes>
+    </Router>
   );
 }
 
