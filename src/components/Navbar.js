369c46e1cb1f700cdc7ef2cec857bc470c13228c
Szabek <mrschabpl@gmail.com>
2022-06-29 17:02:02 +0200

    simple navbar added

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,11 +1,30 @@
 import React from 'react'
+import logo from './img/logo.png'
+
 import { Link } from 'react-router-dom'
 
 const Navbar = () => {
   return (
-    <div>
-      <h2>navbar component</h2>
-    </div>
+      <nav className="navbar navbar-expand-lg navbar-light bg-light">
+          <div className="container-fluid">
+              <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                  <a className="navbar-brand" href="#">
+                      <img src={logo} alt="logo" height={50}/>
+                  </a>
+                  <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+                      <li className="nav-item">
+                          <a className="nav-link" href="#">Statistics</a>
+                      </li>
+                      <li className="nav-item">
+                          <a className="nav-link" href="#">About</a>
+                      </li>
+                  </ul>
+                  <form className="d-flex">
+                      <input className="form-control me-2 border-secondary" type="search" placeholder="Search" aria-label="Search"/>
+                  </form>
+              </div>
+          </div>
+      </nav>
   )
 }
 
